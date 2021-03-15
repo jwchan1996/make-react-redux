4990783f3c2b2e3b1c75e73ffcf3a412afdffd60
jwchan <741755613@qq.com>
2021-03-15 16:49:39 +0800

    feat: 结合 context 和 store

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## example description
 
 ![image](https://ae01.alicdn.com/kf/U8a6a65bc19ef4d1da30743b5d7758227C.jpg)
+
+## 结合 context 和 store
+
+将 store 放到父组件 context 对象中，子组件通过 context 获取 store，调用 store.dispatch() 方法时会调用 store.subscribe() 方法传入的函数，从而在函数中获取更改后的 store.state，然后调用 this.setState() 更改组件对对应的 state，从而让组件重新渲染。
--- src/index.js
+++ src/index.js
@@ -4,7 +4,43 @@ import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
+
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+
 class Index extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  getChildContext () {
+    return { store }
+  }
+
   render () {
     return (
       <div>
