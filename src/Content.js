53227b3efb2b78aeb54ed2a086b0862bae46a076
jwchan <741755613@qq.com>
2021-03-15 15:50:23 +0800

    feat: init

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ThemeSwitch from './ThemeSwitch'
+
+class Content extends Component {
+  render () {
+    return (
+      <div>
+        <p>React.js 小书内容</p>
+        <ThemeSwitch />
+      </div>
+    )
+  }
+}
+export default Content
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Content from './Content'
+import './index.css'
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Index />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
